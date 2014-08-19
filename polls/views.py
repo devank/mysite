3647e3d35216bec082020bec56f4273bf42c4aa8
Ubuntu <ubuntu@ip-172-31-40-141.ec2.internal>
2014-08-19 14:59:59 +0000

    changed the poll app files

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
 
-# Create your views here.
+from polls.models import Question
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = RequestContext(request, {
+        'latest_question_list': latest_question_list,
+    })
+    return HttpResponse(template.render(context))
